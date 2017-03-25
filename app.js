b3744e034fd58784dd491a192562f6edc38eb04d
odulzaides <dulzaides.oscar@gmail.com>
2017-03-25 11:16:15 -0700

    Changed model, controller, view roles. IN PROGRESS

--- app.js
+++ app.js
@@ -1,78 +1,13 @@
-'use strict'
+// Model
 
-////  Model  //// 
-
-// Athlete Object
-const Athlete = function (name, age, username) {
-    this.username = username;
+const Athlete = function(name, age, weight, username, email){
     this.name = name;
     this.age = age;
+    this.weight = weight;
+    this.username = username;
+    this.email = email;
+    this.pwd = pwd;
     this.testResults = [];
-    this.lthr = [];
-    // TODO: Add new Athlete to array
 }
 
-// Athlete methods
-Athlete.prototype = {
-    getName: function () {
-        console.log(this.name);
-    },
-    getAge: function () {
-        console.log(this.age);
-    },
-    setZones: function () {
-        let testResults = this.testResults;
-        let lthrArr = [.81, .81, .89, .90, .93, .94, .99, 1.00, 1.02, 1.03, 1.06, 1.06];
-        let ftpArr = [.55, .56, .75, .76, .85, .95, .96, 1.05, 1.06, 1.20, 1.21, 1.50, 1.51];
-        let calculateZones = function (arr, index) {
-            let zones = arr.map(function (item) {
-                return Math.floor(item * testResults[0].testResult[index]);
-            });
-            return zones;
-        }
-        this.lthrZones = calculateZones( lthrArr, 2);
-        this.powerZones = calculateZones(ftpArr, 1);
-    },
-    // Set ftp and lthr into array
-    setMetrics: function (ftp, lthr) {
-        let date = new Date().toLocaleDateString();
-        let metrics = {};
-        metrics.testResult = [date, ftp, lthr];
-        this.testResults.push(metrics);
-        // console.log(this.testResults);
-
-    }
-
-}
-
-// All Athletes
-const AthleteDatabase = function (athlete) {
-    return function () {
-        let athletes = [];
-        athletes.push(athlete);
-    }
-    // console.log(athletes);   
-}
-
-// Test code
-let oscar = new Athlete("Oscar J Dulzaides", 56, "odulzaides");
-let thomas = new Athlete("Thomas Corea", 56, "thomasc");
-// oscar.setMetrics(230, 152);
-// oscar.setMetrics(230, 152);
-// oscar.setMetrics(230, 152);
-// oscar.setZones();
-// console.log(oscar.lthrZones); 
-// console.log(oscar.powerZones);  
-
-/////  Controller ////
-const SetAthtleteData = function () {
-    // TODO: Add new Athlete to array
-    return;
-}
-
-///////  View /////
-// Make an object
-// 
-const ZoneTable = function () {
-
-}
+Athlete.prototype
